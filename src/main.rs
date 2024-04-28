54cedfb6385aea2a9ad82839b77bbe935441df54
LowLevelCodingCH <135059709+LowLevelCodingCH@users.noreply.github.com>
2024-04-28 15:14:04 +0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use std::fs;
 use std::process::Command;
 use colored::Colorize;
 
-fn main() {
-    let binding = fs::read_to_string("cbuild").expect("No cbuild-file");
-    let code = binding.as_str();
+fn runSection(code: &str){
     let mut gcccmd: Vec<String> = vec!["gcc".to_string()];
 
     for line in code.split('\n') {
         let tokens: Vec<&str> = line.split_whitespace().collect();
         if let Some(token) = tokens.get(0) {
             match *token {
+                "ObjectOut" => {
+                    gcccmd.push("-c".to_string());
+                }
                 "Source" => {
                     if let Some(lib) = tokens.get(1) {
                         gcccmd.push(lib.to_string());
@@ -47,7 +48,18 @@ fn main() {
         .args(&gcccmd[1..]) // Exclude "gcc" from arguments
         .output()
         .expect("Failed to execute command");
+    println!("{}", format!("Executed Command: {:?} After Preferences", gcccmd.join(" ")).yellow().bold());
     println!("status: {}", output.status);
     println!("{}", format!("{} {}", "[~] stdout:", String::from_utf8_lossy(&output.stdout)).yellow().bold());
     println!("{}", format!("{} {}", "[-] stderr:", String::from_utf8_lossy(&output.stderr)).red().bold());
+}
+
+
+fn main() {
+    let binding = fs::read_to_string("cbuild").expect("No cbuild-file");
+    let sections = binding.as_str().split("==~==");
+    for sectionSplitS in sections{
+        let section = format!("{}", sectionSplitS);
+        runSection(&section.as_str());
+    }
 }
